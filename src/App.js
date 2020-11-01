300e2434ea88df350e0eca1bdaf921050e292d17
ephie oyedoh <ephieo@rocketmail.com>
2020-11-01 18:09:04 +0000

    adding redirects file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" component={Creation} />
+          <Route exact path="/" component={Creation} />
         </Switch>
       </Router>
     </div>
